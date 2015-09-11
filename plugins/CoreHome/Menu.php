57bc6efc2e0b7db4401555890ce0b902393402ec
Thomas Steur <thomas.steur@gmail.com>
2015-09-11 20:20:06 +0000

    use icon for username

--- plugins/CoreHome/Menu.php
+++ plugins/CoreHome/Menu.php
@@ -27,24 +27,24 @@ class Menu extends \Piwik\Plugin\Menu
         }
 
         if (Plugin\Manager::getInstance()->isPluginActivated('Feedback')) {
-            $menu->addItem('icon-help', null, array('module' => 'Feedback', 'action' => 'index'), $order = 990);
+            $menu->addItem('icon-help', null, array('module' => 'Feedback', 'action' => 'index'), $order = 990, 'Help');
         }
 
         if (Piwik::isUserIsAnonymous()) {
             if (Plugin\Manager::getInstance()->isPluginActivated('Feedback')) {
-                $menu->addItem($login, null, array('module' => 'Feedback', 'action' => 'index'), 970);
+                $menu->addItem('icon-user', null, array('module' => 'Feedback', 'action' => 'index'), 970, $login);
             } else {
-                $menu->addItem($login, null, array('module' => 'API', 'action' => 'listAllAPI'), 970);
+                $menu->addItem('icon-user', null, array('module' => 'API', 'action' => 'listAllAPI'), 970, $login);
             }
         } else {
-            $menu->addItem($login, null, array('module' => 'UsersManager', 'action' => 'userSettings'), 970);
+            $menu->addItem('icon-user', null, array('module' => 'UsersManager', 'action' => 'userSettings'), 970, $login);
         }
 
         $module = $this->getLoginModule();
         if (Piwik::isUserIsAnonymous()) {
             $menu->addItem('Login_LogIn', null, array('module' => $module, 'action' => false), 1000);
         } else {
-            $menu->addItem('plugins/Morpheus/images/signout.png', null, array('module' => $module, 'action' => 'logout', 'idSite' => null), 1000);
+            $menu->addItem('plugins/Morpheus/images/signout.png', null, array('module' => $module, 'action' => 'logout', 'idSite' => null), 1000, 'Sign out');
         }
     }
 
