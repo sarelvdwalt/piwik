af251da49443498ce32bfc2b6897f7c1a5ee4d6e
Thomas Steur <thomas.steur@gmail.com>
2015-09-18 15:30:35 +0000

    highlight selectors on hover

--- plugins/CoreHome/javascripts/top_controls.js
+++ plugins/CoreHome/javascripts/top_controls.js
@@ -10,7 +10,7 @@ function initTopControls() {
 
     var siteSelector = $('.sites_selector_in_dashboard');
     if (siteSelector.length) {
-        left = $(siteSelector.first()).outerWidth() + 37;
+        left = $(siteSelector.first()).outerWidth() + 27;
     }
 
     if ($topControlsContainer.length) {
